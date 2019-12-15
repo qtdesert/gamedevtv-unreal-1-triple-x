2c245351fecbccbe615676dfcd5385f3665baa1a
Quentin Désert <quentin.desert@gmail.com>
2019-12-16 00:48:40 +0100

    Added input from user

--- triplex.cpp
+++ triplex.cpp
@@ -16,9 +16,16 @@ int main()
     std::cout << std::endl;
     std::cout << "There are 3 numbers in the code" << std::endl;
     std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
-    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl << std::endl;
+    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
 
-    int PlayerGuess;
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
 
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    std::cout << std::endl;
     return 0;
 }
