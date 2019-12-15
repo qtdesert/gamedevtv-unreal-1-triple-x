9b97b6773ab3a75f057846880770a55623add2df
Quentin Désert <quentin.desert@gmail.com>
2019-12-15 23:46:53 +0100

    Su and product

--- triplex.cpp
+++ triplex.cpp
@@ -5,5 +5,17 @@ int main()
     std::cout << "You are a galaxy smuggler breaking into a secure server room to break into a space station.";
     std::cout << std::endl;
     std::cout << "You need to enter the correct codes to continue....\n";
-    return 0;
+
+    int a = 4;
+    int b = 7;
+    int c = 9;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl << std::endl;
+
+return 0;
 }
