4bf9d6dbb08c7fa7ed85c8fdefc16b90bb228753
Quentin Désert <quentin.desert@gmail.com>
2019-12-18 12:55:45 +0100

    Added end lines

--- triplex.cpp
+++ triplex.cpp
@@ -2,9 +2,8 @@
 
 int main()
 {
-    std::cout << "You are a galaxy smuggler breaking into a secure server room to break into a space station.";
-    std::cout << std::endl;
-    std::cout << "You need to enter the correct codes to continue....\n";
+    std::cout << "You are a galaxy smuggler breaking into a secure server room to break into a space station.\n";
+    std::cout << "You need to enter the correct codes to continue....\n\n";
 
     const int CodeA = 4;
     const int CodeB = 5;
@@ -14,25 +13,23 @@ int main()
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     std::cout << std::endl;
-    std::cout << "There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
-    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+    std::cout << "There are 3 numbers in the code";
+    std::cout << "\n+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
 
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
     if (GuessSum == CodeSum && GuessProduct && CodeProduct)
     {
-        std::cout << "You broke the code! The space station is now open." << std::endl;
+        std::cout << "You broke the code! The space station is now open.\n";
     }
     else
     {
-        std::cout << "Wrong code! The space station is still closed." << std::endl;
+        std::cout << "Wrong code! The space station is still closed.\n";
     }
 
     std::cout << std::endl;
